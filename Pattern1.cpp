fb363b31254d237526b78c4cb3575a99a647430d
Ritik Raj <ritikraj2629@gmail.com>
2022-11-08 20:58:25 +0530

    Pattern

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -1,18 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the no: ";
-    cin>>n;
-    int i=1;
-    while(i<=n){
-        int j=1;
-        while(j<=n){
-            cout<<"* ";
+    cout << "Enter the no: ";
+    cin >> n;
+    int i = 1;
+    while (i <= n){
+        int j = 1;
+        while (j <= n){
+            cout << "* ";
             j++;
         }
-    cout<<endl;
+        cout << endl;
         i++;
     }
 }
