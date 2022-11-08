13ff37c8deeb0b9484495883ecc27e738c78a620
Ritik Raj <ritikraj2629@gmail.com>
2022-11-08 21:00:37 +0530

    Update Pattern2.cpp

--- Pattern2.cpp
+++ Pattern2.cpp
@@ -1,24 +1,3 @@
-
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n;
-    cin >> n;
-    int i = 1;
-    while (i <= n){
-        int j = 1;
-        while (j <= n){
-            cout << " " << i;
-            j++;
-        }
-        cout << endl;
-        i++;
-    }
-}
-
-o / p : 
-
 #include<iostream>
 using namespace std;
 
