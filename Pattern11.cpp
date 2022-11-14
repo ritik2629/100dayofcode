539e5e28ca40ea859fb19c4c6cba921238f3acac
Ritik Raj <ritikraj2629@gmail.com>
2022-11-14 18:21:26 +0530

    pattern11

---
+++ Pattern11.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter The no.";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            char ch='A'+j-1;
+            cout<<ch<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+pattern:
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
