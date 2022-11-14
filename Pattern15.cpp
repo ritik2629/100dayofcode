248a0da781782112b942c937420203ec06a75ab5
Ritik Raj <ritikraj2629@gmail.com>
2022-11-14 19:41:49 +0530

    Pattern15

---
+++ Pattern15.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter The no ";
+    cin>>n;
+    int i=1;
+    int count=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            char ch =64+count;
+            cout<<ch<<" ";
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+pattern:
+A 
+B C 
+D E F 
+G H I J 
+K L M N O 
