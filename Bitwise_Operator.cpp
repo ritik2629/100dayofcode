ea293da78693d6f222e712d069176e0814696295
Ritik Raj <ritikraj2629@gmail.com>
2022-11-15 09:00:34 +0530

    Bitwise operator

---
+++ Bitwise_Operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=4;
+    int b=6;
+    cout<< "a & b :"<<(a&b)<<endl;
+    cout<< "a | b :"<<(a|b)<<endl;
+    cout<< "~a :"<<(~a)<<endl;
+    cout<< "a ^ b :"<<(a^b)<<endl;
+}
+
+
+o/p-
+
+a & b :4
+a | b :6
+~a :-5
+a ^ b :2
